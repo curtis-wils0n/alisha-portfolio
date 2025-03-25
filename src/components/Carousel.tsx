fc1c376950ea5bd1228d0599c6a033cce8123ee2
Curtis Wilson <curtis.wils0n@protonmail.com>
2025-03-24 21:48:31 -0600

    Add new projects

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -2,30 +2,26 @@ import {
   Carousel as BaseCarousel,
   CarouselContent,
   CarouselItem,
-  CarouselPrevious,
-  CarouselNext,
 } from "@/components/ui/carousel";
 
 interface Props {
   data: {
-    src: string
+    src: string;
   }[];
 }
 
 export function Carousel({ data }: Props) {
   return (
-    <BaseCarousel opts={{loop: true}} className="w-full h-full max-w-6xl">
+    <BaseCarousel opts={{ loop: true }} className="w-full h-full max-w-6xl">
       <CarouselContent>
         {data.map((item) => {
           return (
-            <CarouselItem className="md:basis-1/2 lg:basis-1/3">
-                <img src={item.src} /> 
+            <CarouselItem className="md:basis-1/2 lg:basis-1/3" key={item.src}>
+              <img src={item.src} />
             </CarouselItem>
-          )
+          );
         })}
       </CarouselContent>
-      <CarouselPrevious />
-      <CarouselNext />
     </BaseCarousel>
   );
 }
